e4a0b743ad54c4c55abb2195c638451079688dd7
FERHATSUMER <sumerf39@gmail.com>
2023-05-10 20:08:17 +0300

    state-react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Course from "./Course";
+
+function getRandomCourse() {
+  const courseArray = [
+    "Bmw",
+    "Camaro",
+    "Ferrari",
+    "Fiat",
+    "Honda",
+    "Mercedes",
+    "Nissan",
+    "Pejo",
+    "Togg",
+  ];
+ return getRandomCourse[Math.floor(Math.random() * courseArray.length)];
+}
 
 function App() {
+  const [courses, setCourses] = useState([]);
+
+  const handleClick = () => {
+    setCourses([...courses, getRandomCourse])
+  };
   return (
     <div className="App">
- 
+      <button onClick={handleClick}>Araba Ekle </button>
+      <Course />
     </div>
   );
 }
---
+++ src/Course.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Bmw from './images/Bmw.jpg'
+
+const Course = () => {
+
+  return (
+    <div>
+      <img src={Bmw} alt='' />
+    </div>
+  )
+}
+
+export default Course
